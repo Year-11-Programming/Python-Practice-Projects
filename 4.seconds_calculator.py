89c384c2d54c8d1f4e6bd74fa08f3532dbb9f903
Rachel Scott WSC <150975983+RScottWSC@users.noreply.github.com>
2026-04-24 08:41:05 +1200

    lesson 2 (and some updates)

---
+++ 4.seconds_calculator.py
@@ -0,0 +1,22 @@
+# Create a calculator that asks the user for a number (of days)
+# and outputs how many seconds in that number of days
+
+# Values - start by writing constants to hold:
+# The number of seconds in a minute
+
+# The number of minutes in an hour
+
+# The number of hours in a day
+
+
+# Get input from the user and save it in a variable
+
+# Change the value into an integer and resave in the variable
+
+
+# Calculate the number of seconds using * with the input and your constants. 
+# Save it in a new variable.
+
+# Output the answer
+
+# ---------------------------------
--- _main.py
+++ _main.py
@@ -1,1 +1,9 @@
 # Use this file to investigate different lines of code.
+MULT_NUM = 5
+INSTRUCTIONS = "Choose a number and I will multiply it by " + str(5)
+
+name = input("Hi! What's your name?")
+print("Hi", name)
+print(INSTRUCTIONS)
+num = input("What number should I multiply?")
+print("The answer is:", int(num) * MULT_NUM)
