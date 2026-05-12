6172582155eb97eb78465cceb4ac60e903939be4
Rachel Scott WSC <rachscottnz@gmail.com>
2026-05-13 10:38:24 +1200

    Create debugging_5.py

---
+++ Lesson 5/debugging_5.py
@@ -0,0 +1,16 @@
+user_age = input("Enter your age: ")
+if user_age < 18
+discount = 20.0
+else userAge == 18:
+discount = 10.0
+else
+discount = 0.0
+item_price = 50
+final_price = item_price - discount
+gst = 0.12
+final_price = final_price * gst
+print("The total is: " + finalprice)
+IF finalprice is greater than 40 THEN 
+    OUTPUT "Expensive"
+OTHERWISE 
+    OUTPUT "Affordable"
