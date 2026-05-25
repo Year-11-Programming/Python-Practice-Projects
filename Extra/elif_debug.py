6b205efcdfe3d1d3843b9a64a4136555d273103b
Rachel Scott WSC <rachscottnz@gmail.com>
2026-05-25 14:44:45 +1200

    Create elif_debug.py

---
+++ Extra/elif_debug.py
@@ -0,0 +1,35 @@
+# --- THE TRAFFIC LIGHT SIMULATOR ---
+
+# Section 1
+light_color = "Yellow"
+
+if light_color == "Red":
+    print("STOP!")
+elif light_color = "Yellow":
+    print("SLOW DOWN!")
+else:
+    print("GO!")
+
+
+# Section 2
+driver_speed = 45
+
+if driver_speed > 50:
+    print("You are speeding! Ticket issued.")
+else:
+    print("Safe speed. Have a good day!")
+elif driver_speed < 40:
+    print("You're slowing traffic.")
+
+
+# Section 3
+score = 85
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70
+    print("Grade: C")
+else
+    print("Grade: F")
