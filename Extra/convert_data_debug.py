86957255aa8a50f1d50b53f740581d8052bf6d89
Rachel Scott WSC <rachscottnz@gmail.com>
2026-05-25 15:27:04 +1200

    Create convert_data_debug.py

---
+++ Extra/convert_data_debug.py
@@ -0,0 +1,18 @@
+# --- THE GAMER STATS CALCULATOR ---
+
+# Section 1
+user_age = input("Enter your age: ")
+years_left = 100 - user_age
+print("You will turn 100 in " + years_left + " years!")
+
+
+# Section 2
+spawn_rate = "5"
+multiplier = 3
+total_enemies = int(spawn_rate * multiplier)
+print("Total enemies spawning: " + str(total_enemies))
+
+
+# Section 3
+score = 950
+print("Your high score is: " + score)
