3fc7cadc6b58b6279ddf5df2d905ef8ce451f626
Rachel Scott WSC <rachscottnz@gmail.com>
2026-05-25 16:12:57 +1200

    Create string_method_debug.py

---
+++ Extra/string_method_debug.py
@@ -0,0 +1,18 @@
+# --- THE GUESTLIST CLEANUP ---
+
+# Section 1
+entered_code = "   VIP-PASS   "
+clean_code = entered_code.strip
+print("Your clean passcode is: " + clean_code)
+
+
+# Section 2
+warning = "danger ahead"
+shouted_warning = upper(warning)
+print("ALERT: " + shouted_warning)
+
+
+# Section 3
+guest_name = "ALEX SMITH"
+lowercase_name = guest_name.lower("ALEX SMITH")
+print("Logged name: " + lowercase_name)
