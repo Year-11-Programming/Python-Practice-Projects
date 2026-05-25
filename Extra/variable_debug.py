1b363a306834a7c058460d3a5f12b5c35d1a92e4
Rachel Scott WSC <rachscottnz@gmail.com>
2026-05-25 14:00:35 +1200

    Create variable_debug.py

---
+++ Extra/variable_debug.py
@@ -0,0 +1,20 @@
+# --- THE HERO'S INVENTORY ---
+
+# Section 1
+hero name = input("What's your name hero?")
+gold_found = 50
+gold_lost = 20
+current_gold = Golf_Found - "gold_lost"
+
+
+# Section 2
+weapon = "Sword"
+damage = 15
+level_up_bonus = 5
+total_damage = damage + level-up-bonus
+
+
+# Section 3
+print("Hero Name: " + hero_name)
+print("Gold Coins: " + "current_gold")
+print("Weapon Damage: " total_damage)
