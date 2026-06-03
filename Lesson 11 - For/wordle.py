1a770bf1a8136ff4ae91cd67f167ace15d75e705
Rachel Scott WSC <rachscottnz@gmail.com>
2026-06-03 14:48:38 +1200

    Update wordle.py

--- Lesson 11 - For/wordle.py
+++ Lesson 11 - For/wordle.py
@@ -25,28 +25,27 @@
 
     # TODO Check if they got it correct and if they did, tell them so and then break the loop
 
-    # TODO Create a variable count and set it to 0
-    # TODO Create a while loop that goes while count is less than 5
-        # TODO Check if the 'count' letter of user_input is the same as the 'count' letter of the word and if it is tell them they got that letter correct
+    # TODO Create a for loop that loops 5 times
+        # TODO Check if the current letter of user_input (user_input[i]) is the same as the i letter of the word and if it is tell them they got that letter correct
 
-        # TODO Otherwise check if the 'count' letter of user_input is in the word and if it is, tell them that letter is in the wrong position
+        # TODO Otherwise check if the current letter of user_input is in the word and if it is, tell them that letter is in the wrong position
 
         # TODO Else tell them that letter is wrong
-
-        # TODO Increase count by 1
 
 # TODO Ask if they want to play again. If they don't, set play to false.
 
 
 # ==========================================================
 # EXTENSION
-# Instead of telling the user one by one about their letters, put each correct letter or _ for a wrong letter into a list. 
+# Instead of telling the user one by one about their letters, put each correct letter and _ for a wrong letter into a list. 
 # Then finally print the list (you can use "".join(list_name) to merge them into a string if you like)
 
 # ==========================================================
 # EXPERT
-# Following on from the extension, add colour to the letters instead (Don't use _ for incorrect anymore). Green for correct, orange for wrong place, red for incorrect.
+# Following on from the extension, add colour to the letters instead (Don't use _ for incorrect anymore). Green for correct, orange for wrong place, red for incorrect. You'll need to add the colour as you add them to the list
 
 # print("\033[31mThis is Red Text\033[0m")
 # print("\033[38;2;255;165;0mThis is Orange Text\033[0m")
 # print("\033[32mThis is Green Text\033[0m")
+
+# Further Extension: Structure with user defined functions
