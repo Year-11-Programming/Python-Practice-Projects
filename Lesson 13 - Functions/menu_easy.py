daeafc54b64a426243d078fdbb9aef3b85348783
Rachel Scott WSC <rachscottnz@gmail.com>
2026-06-15 16:06:42 +1200

    lesson 13 project

---
+++ Lesson 13 - Functions/menu_easy.py
@@ -0,0 +1,63 @@
+"""
+PROGRAM: Menu
+This starts with a menu so users can run 1 of 3 different programs:
+1.
+2.
+3.
+"""
+
+#===============================
+# IMPORTS
+#===============================
+
+# TODO Once you've copied your 3 programs below, move any imports to here
+
+#===============================
+# FUNCTIONS
+#===============================
+
+# TODO Create a function to hold your first program. You can call it run_program_name(). 
+    # TODO Copy and paste the programs code inside (meaning indented)
+
+
+# TODO Create a function to hold your second program. You can call it run_program_name(). 
+    # TODO Copy and paste the programs code inside (meaning indented)
+
+
+# TODO Create a function to hold your third program. You can call it run_program_name(). 
+    # TODO Copy and paste the programs code inside (meaning indented)
+
+
+# TODO Create a main function
+
+    # TODO Introduce your menu, including outputting all three options with numbers
+    
+    # TODO Ask user which program they'd like to run and store input in a variable
+
+    # TODO Check if they said "1" (with speech marks)
+
+        # TODO Call program 1 function
+
+    # TODO Otherwise check if they said "2" (with speech marks)
+
+        # TODO Call program 2 function
+
+    # TODO Otherwise check if they said "3" (with speech marks)
+
+        # TODO Call program 3 function
+
+
+#===============================
+# EXECUTION
+#===============================
+
+# TODO Call main function
+
+
+
+#===============================
+#===============================
+# EXTENSION
+# TODO Go back to each program you chose and structure them with functions. 
+# TODO Then recopy them over as multiple functions (rather than one)
+# NOTE The main() function in your programs can be renamed as run_program_name() so it doesn't clash with this program's main()
